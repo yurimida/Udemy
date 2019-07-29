14f6fafd9fb200ed87da50285ab6c2e2f4375ff0
multicampus <yurim.l2068@gmail.com>
2019-07-29 20:00:25 +0900

    second

--- diary/src/components/App.js
+++ diary/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {database} from '../firebase';
 
 class App extends Component {
 
@@ -8,24 +9,36 @@ class App extends Component {
       title: '',
       body: '',
     };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleChange(e){
     this.setState({
       [e.target.name] : e.target.value
     });
+  } 
+
+  handleSubmit(e){
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body,
+    }
+    database.add(note)
   }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit = {this.handleSubmit}>
               <div className="form-group">
-                <input onChange={this.handleChange} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
+                <input value={this.state.title} onChange={this.handleChange} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
               </div>
               <div className="form-group">
-                <textarea onChange={this.handleChange} type="text" name="body" className="form-control no-border" placeholder="Body..." required />
+                <textarea value={this.state.body} onChange={this.handleChange} type="text" name="body" className="form-control no-border" placeholder="Body..." required />
               </div>
               <div className="form-group">
                 <button className="btn btn-primary col-xs-12">Save</button>
--- diary/src/firebase.js
+++ diary/src/firebase.js
@@ -5,12 +5,13 @@ import * as firebase from 'firebase'
     authDomain: "diary-1f7d6.firebaseapp.com",
     databaseURL: "https://diary-1f7d6.firebaseio.com",
     projectId: "diary-1f7d6",
-    storageBucket: "",
+    storageBucket: "diary-1f7d6.appspot.com",
     messagingSenderId: "155152380669",
     appId: "1:155152380669:web:8cf8f49d5f796a7b"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  export const database = firebase.database().ref('/notes')
+//   export const database = firebase.database().ref('/posts')
+  export const database = firebase.firestore().collection('posts')
   
